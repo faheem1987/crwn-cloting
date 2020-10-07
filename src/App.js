7e168f57eb15270850a29510581ffb55cf2ae295
Faheem Ahmad <fahmad@walmartlabs.com>
2020-10-07 02:16:47 -0700

    adding header component and links to home, shope page and contact

--- src/App.js
+++ src/App.js
@@ -5,14 +5,16 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage';
 import Shop from './pages/shop/shop';
+import Header from './components/header/header';
 
 function App() {
   return (
     <div>
-    <Switch>
-      <Route exact path='/' component={HomePage} />
-      <Route exact path='/shop' component={Shop} />
-    </Switch>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop' component={Shop} />
+      </Switch>
     </div>
   );
 }
